ccf3580b0bc259374079f60c3f69218d9f11c6ad
jaredoneal <joneal12@uncc.edu>
2016-12-06 01:03:29 -0500

    changed jpg to JPG
    changed jpg to JPG

--- slickmemes/js/app.js
+++ slickmemes/js/app.js
@@ -3,34 +3,34 @@ function CarouselDemoCtrl($scope){
   $scope.myInterval = 3000;
   $scope.slides = [
     {
-      image: '/photos/KohChang.jpg',
+      image: '/photos/KohChang.JPG',
       text: 'Koh Chang, Thailand'
     },
     {
-      image: '/photos/Scotland.jpg',
+      image: '/photos/Scotland.JPG',
       text: 'Isle of Skye, Scotland'
     },
     {
-      image: '/photos/Charlotte3.jpg',
+      image: '/photos/Charlotte3.JPG',
       text: 'Uptown Charlotte, NC'
     },
     {
-      image: '/photos/PonteVecchio.jpg',
+      image: '/photos/PonteVecchio.JPG',
       text: 'Florence, Italy'
     }
     ,
     {
-      image: '/photos/Miami.jpg',
+      image: '/photos/Miami.JPG',
       text: 'Miami Beach, FL'
     }
     ,
     {
-      image: '/photos/Manhattan.jpg',
+      image: '/photos/Manhattan.JPG',
       text: 'New York City'
     }
     ,
     {
-      image: '/photos/Tampa2.jpg',
+      image: '/photos/Tampa2.JPG',
       text: 'Tampa, FL'
     }
   ];
