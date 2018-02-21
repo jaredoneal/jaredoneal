cad7b179611b23aabaabbd00243c84276b73c3ca
jaredoneal <joneal12@uncc.edu>
2018-02-20 20:53:19 -0600

    more zoo changes

--- test/js/petSelector.js
+++ test/js/petSelector.js
@@ -12,3 +12,16 @@ function togglePets(id) {
     document.getElementById(id).style.display = "block";
   }
      
+  function toggleMarble(id) {
+    document.getElementById("good-button").classList.remove("pulse");
+    document.getElementById("bad-button").classList.remove("pulse");
+    document.getElementById("ugly-button").classList.remove("pulse");
+    const marbleEl = document.getElementsByClassName("marbles");
+    for(var i = 0; i < marbleEl.length; i++) {
+        marbleEl[i].style.display = "none";
+    }
+    
+    document.getElementById(id + "-button").classList.add("pulse");
+    document.getElementById(id).style.display = "block";
+  }
+    
